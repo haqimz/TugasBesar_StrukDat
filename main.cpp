cf8e142b79af80c51f90d1baea94ccb78ef14087
Petriichor <86548491+Petriichor@users.noreply.github.com>
2025-12-19 22:29:16 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,55 @@
-#include "types.h"
+#include <iostream>
+#include <string>
 #include "library.h"
 #include "playlist.h"
+#include "user.h"
 #include "player.h"
 
-int main() {
+using namespace std;
 
+int main() {
+    Library myLibrary;
+    initLibrary(myLibrary);
+
+    Sistem mySistem;
+    initSistem(mySistem);
+
+    Player myPlayer;
+    initPlayer(myPlayer);
+
+    registerAdmin(mySistem, "user1", "123");
+
+    int mainChoice;
+    do {
+        cout << "\n=======================================" << endl;
+        cout << "   WELCOME TO JETT REVIVE ME PLAYER    " << endl;
+        cout << "=======================================" << endl;
+        cout << "1. Login" << endl;
+        cout << "2. Register User Baru" << endl;
+        cout << "0. Keluar Aplikasi" << endl;
+        cout << "---------------------------------------" << endl;
+        cout << "Pilih menu: ";
+        cin >> mainChoice;
+
+        switch (mainChoice) {
+            case 1:
+                menuLogin(mySistem, myLibrary, myPlayer);
+                break;
+            case 2: {
+                string u, p;
+                cout << "Masukkan Username baru: "; cin >> u;
+                cout << "Masukkan Password baru: "; cin >> p;
+                registerAdmin(mySistem, u, p);
+                cout << "User berhasil terdaftar!" << endl;
+                break;
+            }
+            case 0:
+                cout << "Terima kasih telah menggunakan aplikasi ini." << endl;
+                break;
+            default:
+                cout << "Pilihan tidak valid." << endl;
+        }
+    } while (mainChoice != 0);
+
+    return 0;
 }
